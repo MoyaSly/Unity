891feccce52c97dbd7914381a8d620137376c270
Miquel Moya Sanjuán <miquelmoyasan@hotmail.com>
2016-10-14 09:51:42 +0200

    PERS

---
+++ New Unity Project 1/Assets/personaje.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class personaje : MonoBehaviour {
+
+    public NavMeshAgent nav_mesh_agent;
+    public Animator animator;
+    
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (nav_mesh_agent.remainingDistance > nav_mesh_agent.radius)
+        {
+            animator.SetBool("mov", true);
+        }
+    }
+}
